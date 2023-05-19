6afbf8e1178f74fc972b5d7df3c438edd66c0638
Rahul Gautam <117631348+UserRahulrg@users.noreply.github.com>
2023-05-19 20:50:15 +0530

    Update

---
+++ if_else.py
@@ -0,0 +1,46 @@
+Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+def function_2():
+    a = 10
+    x = 4
+    b = 20
+...     c = 30
+...     value = 0
+...     value = (a * (x*x)) + (b * x) + c
+...     print('value of ax*x+bx+c is:',value)
+... 
+...     
+>>> function_2()
+value of ax*x+bx+c is: 270
+>>> 10*16+80+30
+270
+>>> 
+>>> a = 10
+>>> b = 20
+>>> c = 30
+>>> 
+>>> if a > c:
+...     print('A is Greatest')
+... 
+...     
+>>> if a > c:
+...     print('A is Greatest')
+... elif b > c:
+...     print('B is Greatest')
+... else:
+...     print('C is Greatest')
+... 
+...     
+C is Greatest
+>>> 
+>>> a = 100
+>>> 
+>>> if a / 5:
+...     if a / 2:
+...         print('A is divisible by both 2 and 5')
+... else:
+...     print('A is not divisible by 2 and 5')
+... 
+...     
+A is divisible by both 2 and 5
+>>> 
